d073241b9fd2f5f375a4d778fbb2ae678d5daeb3
Sidneyw <sidney.wijngaarde1@ibm.com>
2018-05-08 08:46:43 -0400

    Build fix

--- projects/frontend/src/layouts/experience.js
+++ projects/frontend/src/layouts/experience.js
@@ -5,18 +5,27 @@ import Helmet from 'react-helmet';
 
 import Footer from '../components/Footer';
 
+import './index.css';
+
 const TemplateWrapper = ({ children, data }) => (
   <div>
     <Helmet>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+      />
       <link
-        href="https://fonts.googleapis.com/css?family=Roboto+Slab:100,400|Ubuntu:400,700"
+        href="https://fonts.googleapis.com/css?family=Roboto+Slab:100,300,400|Ubuntu:400,700"
         rel="stylesheet"
       />
     </Helmet>
 
     {children()}
 
-    <Footer siteMetadata={data.site.siteMetadata} />
+    <Footer
+      siteMetadata={data.site.siteMetadata}
+      images={data.allImageSharp.edges}
+    />
   </div>
 );
 
@@ -33,7 +42,20 @@ export const query = graphql`
   query ExperienceQuery {
     site {
       siteMetadata {
-        title
+        social {
+          name
+          link
+        }
+      }
+    }
+    allImageSharp(filter: { id: { regex: "/.*assets/social/.*/" } }) {
+      edges {
+        node {
+          id
+          sizes {
+            ...GatsbyImageSharpSizes_withWebp
+          }
+        }
       }
     }
   }
